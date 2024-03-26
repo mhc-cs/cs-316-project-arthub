ed04fc9cf5d74f52af4b9169035257f154832537
golov22o <golov22o@cs-vm-04.cs.mtholyoke.edu>
2024-03-26 14:35:04 -0400

    main commit first

---
+++ src/components/Modal.tsx
@@ -0,0 +1,11 @@
+// function Modal() {
+//     const [open, setOpen]
+
+//     return (
+//         <div>
+//             <h1>I am a modal</h1>
+//         </div>
+//     );
+// }
+
+// export default Modal
